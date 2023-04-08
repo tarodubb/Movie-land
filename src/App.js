304cd1e3faecfc09482740b28e31a60c3ac0cb4c
maxwell wong <midorikawamaxwell@gmail.com>
2023-04-08 13:39:48 +0900

    connected and called OMDb api key for movies

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
-import './App.css';
+import { useEffect } from 'react';
+
+// 408e1a5b
+
+const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=408e1a5b'
 
 const App = () => {
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    console.log(data.Search);
+  }
+
+  useEffect(() => {
+    searchMovies('joker');
+  }, []);
+
   return (
-    <div className='App'>
-      <h1>Hello, React</h1>
+    <div>
+      <h1>App</h1>
     </div>
   );
 };
