abbcaa75da4d1e5643476a73853c8ed3230e8524
maxwell wong <midorikawamaxwell@gmail.com>
2023-04-08 14:27:13 +0900

    made one movie card visible

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { useEffect } from 'react';
 
-// 408e1a5b
+import './App.css';
+import SearchIcon from './search.svg'
 
 const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=408e1a5b'
 
-const App = () => {
+const movie1 = {
+  "Title": "Batman Beyond: Return of the Joker",
+  "Year": "2000",
+  "imdbID": "tt0233298",
+  "Type": "movie",
+  "Poster": "https://m.media-amazon.com/images/M/MV5BNmRmODEwNzctYzU1MS00ZDQ1LWI2NWMtZWFkNTQwNDg1ZDFiXkEyXkFqcGdeQXVyNTI4MjkwNjA@._V1_SX300.jpg"
+}
 
+const App = () => {
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
@@ -19,8 +27,39 @@ const App = () => {
   }, []);
 
   return (
-    <div>
-      <h1>App</h1>
+    <div className='App'>
+      <h1>MovieLand</h1>
+      <div className='search'>
+        <input
+          placeholder='Search for Movies'
+          value='superman'
+          onChange={() => {}}
+        />
+        <img
+          src={SearchIcon}
+          alt='search'
+          onClick={() => {}}
+        >
+
+        </img>
+      </div>
+
+      <div className='container'>
+        <div className='movie'>
+          <div>
+            <p>{movie1.Year}</p>
+          </div>
+
+          <div>
+            <img src={movie1.Poster !== 'N/A' ? movie1.Poster : 'https://via.placeholder.com/400'} alt={movie1.title} />
+          </div>
+
+          <div>
+            <span>{movie1.Type}</span>
+            <h3>{movie1.Title}</h3>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
