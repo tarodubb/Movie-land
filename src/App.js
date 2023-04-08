d55d1a59cb73b8cac771ce0de45f74a2ca0d16ea
maxwell wong <midorikawamaxwell@gmail.com>
2023-04-08 14:53:01 +0900

    added a searchTerm state that woudl dynamically pass the searchTerm to search Movies function when the search icon is clicked

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ const movie1 = {
 
 const App = () => {
   const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState([]);
 
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
@@ -35,31 +36,30 @@ const App = () => {
       <div className='search'>
         <input
           placeholder='Search for Movies'
-          value='superman'
-          onChange={() => {}}
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
         />
         <img
           src={SearchIcon}
           alt='search'
-          onClick={() => {}}
+          onClick={() => searchMovies(searchTerm)}
         >
 
         </img>
       </div>
 
-      {
-        movies?.length > 0
-          ?(
-          <div className='container'>
-            {movies.map((movie) => (
-              <MovieCard movie={movie} />
-            ))}
+      {movies?.length > 0
+        ?(
+        <div className='container'>
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+        ) : (
+          <div className='empty'>
+            <h2>No Movies</h2>
           </div>
-          ) : (
-            <div className='empty'>
-              <h2>No Movies</h2>
-            </div>
-          )
+        )
       }
 
 
